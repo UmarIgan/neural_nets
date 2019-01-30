c75718a4fec0b2edb8162706509ff55a1628279d
Umar <38042220+pytmar@users.noreply.github.com>
2019-01-30 01:54:17 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-x=np.random.randn(70, 70)
-y=np.random.randn(70,)          
+x=np.random.randnom_sample((7000, 7000))
+y=np.random.random((7000,))         
 dd=deepnetwork(x, y)
-dd.train(x, y, 50)
+dd.train(x, y, 20)
