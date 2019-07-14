528d7c1dd15854a4f56a8449f8bb7d3dc1f843be
Umar <38042220+pytmar@users.noreply.github.com>
2019-07-14 21:21:58 +0000

    Update deepnetwork.py
    I just added some more paramater and soon i will add more.

--- deepnetwork.py
+++ deepnetwork.py
@@ -1,15 +1,17 @@
 import numpy as np
 class deepnetwork(object):
-    def __init__(self, x, y):
+    def __init__(self, x, y, learning_rate=0.001, bias=0.0):
         self.x=x
         self.y=y
+        self.learning_rate=learning_rate #default the learning rate
+        self.bias=bias
         self.W1=np.zeros(y.shape)#shape of weigths must match with shape of y
         self.W2=np.zeros(y.shape)
         self.W3=np.zeros(y.shape)
         
         
     #FORWARD WİTH 2 HİDDEN LAYERS
-    def forward(self, x, bias=0.0):
+    def forward(self, x):
         self.bias=bias
         """
         y=W*x + b this is as we now a linear regression which is the root of neural network
@@ -19,17 +21,15 @@ class deepnetwork(object):
         """
         self.l1=self.relu(np.dot(self.W1, self.x) + bias)#you can change the activation function parameters to find best loss
         self.l2=self.tanh(np.dot(self.W2, self.l1) + bias)#Please change activation to find best for you
-        o=np.dot(self.W3, self.l2) + bias
+        o=np.dot(self.W3, self.l2) +  self.bias*self.learning_rate
         return o
   
     
     #LOSS FUNCTİONS                  
     def loss_func(self, y):
         self.y=y
-        return(np.mean(np.square(self.y - self.forward(x, 0.9))))
-    def cross_ent_coss(self, y):#not sure if this work correctly
-        o=self.forward(x, 0.9)
-        return -1*np.sum(y*np.log(o))
+        return(np.mean(np.power(self.y - self.forward(x), 2))) #mse error function
+    
 
     #ACTİVATİON FUNCTIONS    
     def softmax(self, A):
@@ -45,7 +45,7 @@ class deepnetwork(object):
                        
     #BACKWARD                   
     def backward(self, x, y, o):
-        self.o_error = np.subtract(y, o)  # error in output
+        self.o_error = self.loss_func(y)  # error in output
         self.o_delta = self.o_error *self.sigmoidprime(o)  
         self.z2_error = np.dot(self.o_delta, self.W2.T)  
         self.z2_delta = self.z2_error * self.sigmoidprime(self.l1) 
@@ -53,12 +53,12 @@ class deepnetwork(object):
         self.z3_delta=self.z3_error*self.sigmoidprime(self.l2)
         self.W1 += np.dot(self.x.T, self.z2_delta)  
         self.W2 += np.dot(self.l1.T, self.z3_delta) 
-        self.W3 += np.dot(self.l2.T, self.o_delta)
+        self.W3 += np.dot(self.l2.T, self.o_delta)*self.learning_rate
     
     def train(self, x, y, epochs):
         self.epochs=epochs
         for epoch in range(epochs):
             pred = self.forward(x, 0.9)
-            self.backward(x, y, pred)
+            self.backward(x, y, o)
             loss=self.loss_func(y)
-            print("accuracy", loss)
+            print(f” accuracy at epoch {epoch} is %{100-loss})
